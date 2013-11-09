9567af3e47b67ac2e1d4c31bef268adce2e31a56
Niklas Damberg <niklas.damberg@gmail.com>
2013-11-09 15:31:00 +0100

    Added Point.h, removed Vector3f.h

---
+++ MonoEngine/Math/Point.h
@@ -0,0 +1,14 @@
+//
+//  Point.h
+//  King_worksample_OSX
+//
+//  Created by Niklas Damberg on 09/11/13.
+//
+//
+
+#ifndef King_worksample_OSX_Point_h
+#define King_worksample_OSX_Point_h
+
+
+
+#endif
