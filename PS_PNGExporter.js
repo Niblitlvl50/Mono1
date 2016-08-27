567ae35e80c9839a315a119fe92b6aedfba8d172
Niklas Damberg <niklas.damberg@gmail.com>
2016-08-28 00:47:22 +0200

    Added a new PS script for exporting png's

---
+++ PS_PNGExporter.js
@@ -0,0 +1,25 @@
+
+var exportDocumentToPNG = function(document, output_file)
+{
+	var exportFile = new File(output_file)
+	var exportOptions = new ExportOptionsSaveForWeb()
+	exportOptions.format = SaveDocumentType.PNG
+	exportOptions.PNG8 = false
+	var result = document.exportDocument(exportFile, ExportType.SAVEFORWEB, exportOptions)
+
+    alert(result)
+}
+
+// Output directory
+var outputPath = "/Users/niklasdamberg/Desktop/work/"
+
+var localDocument = app.activeDocument
+var dotPsdIndex = localDocument.name.search(".psd")
+var textureName = localDocument.name.substr(0, dotPsdIndex) + ".png"
+
+var output_file = outputPath + textureName
+
+exportDocumentToPNG(localDocument, output_file)
+
+// Show alert for user that something has been done
+alert("Successfully exported image!\nOutput folder: " + output_file)
