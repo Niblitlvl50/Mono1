c5d394facccc85ce90cee1ac7a105ffaf46a998f
Niklas Damberg <niklas.damberg@gmail.com>
2016-11-08 22:13:36 +0100

    Added imgui icon helper

---
+++ Shared/ImGuiImpl/ImGuiUtils.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "Math/Quad.h"
+#include "imgui/imgui.h"
+
+struct ImageCoords
+{
+    ImVec2 uv1;
+    ImVec2 uv2;
+};
+
+ImageCoords QuadToImageCoords(const math::Quad& input)
+{
+    ImageCoords coords;
+    coords.uv1 = ImVec2(input.mA.x, input.mB.y);
+    coords.uv2 = ImVec2(input.mB.x, input.mA.y);
+
+    return coords;
+}
