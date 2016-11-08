fe3632ffe1d42f192b985c0719f8323430b27773
Niklas Damberg <niklas.damberg@gmail.com>
2016-11-08 22:15:11 +0100

    Removed comments

--- Editor/Tools/RotateTool.h
+++ Editor/Tools/RotateTool.h
@@ -17,17 +17,12 @@ namespace editor
     {
     public:
 
-        //RotateTool(EditorZone* editor);
-
         virtual void Begin();
         virtual void End();
         virtual bool IsActive() const;
         virtual void HandleMouseDown(const math::Vector2f& world_pos, mono::IEntityPtr entity);
         virtual void HandleMouseUp(const math::Vector2f& world_pos);
         virtual void HandleMousePosition(const math::Vector2f& world_pos);
-
-        //EditorZone* m_editor;
-        //std::shared_ptr<editor::PolygonEntity> m_polygon;
 
         mono::IEntityPtr m_entity;
         float m_rotationDiff;
