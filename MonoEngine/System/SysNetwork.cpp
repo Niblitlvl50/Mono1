42075d91e27db5d95784d7555f7b5b1030ce9545
Niklas Damberg <niklas.damberg@gmail.com>
2016-12-07 21:48:09 +0100

    Broadcast sockets

--- MonoEngine/System/SysNetwork.cpp
+++ MonoEngine/System/SysNetwork.cpp
@@ -8,13 +8,14 @@
 
 #include "SysNetwork.h"
 #include <stdexcept>
-#include <string>
+#include <cstdio>
 
 #define ZED_NET_IMPLEMENTATION
 #define ZED_NET_STATIC
 
 #include "zed_net/zed_net.h"
-
+#include <ifaddrs.h>
+#include <net/if.h>
 
 namespace
 {
@@ -24,10 +25,22 @@ namespace
 
         UDPSocket(const char* address, int port, bool blocking)
         {
-            zed_net_get_address(&m_address, address, port);
+            const int address_result = zed_net_get_address(&m_address, address, port);
+            if(address_result != 0)
+                throw std::runtime_error("Unable to resolve host address");
+
             m_handle = zed_net_udp_socket_open(port, blocking);
             if(!m_handle)
-                throw std::runtime_error("Unable to open UDP socket, error: " + std::string(zed_net_get_error()));
+                throw std::runtime_error("Failed to create socket");
+
+            constexpr int yes = 1;
+            const int result1 = setsockopt(m_handle->handle, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+            if(result1 == -1)
+                throw std::runtime_error("Unable to set reuse address option");
+
+            const int result2 = setsockopt(m_handle->handle, SOL_SOCKET, SO_BROADCAST, &yes, sizeof(yes));
+            if(result2 == -1)
+                throw std::runtime_error("Unable to set broadcast option");
         }
 
         ~UDPSocket()
@@ -41,6 +54,18 @@ namespace
             return zed_net_udp_socket_send(m_handle, m_address, data.data(), size);
         }
 
+        int SendTo(const std::vector<byte>& data, const char* address, int port)
+        {
+            const int size = static_cast<int>(data.size());
+
+            zed_net_address_t socket_address;
+            const int address_result = zed_net_get_address(&socket_address, address, port);
+            if(address_result != 0)
+                return -1;
+
+            return zed_net_udp_socket_send(m_handle, socket_address, data.data(), size);
+        }
+
         void Receive(std::vector<byte>& data)
         {
             zed_net_address_t sender;
@@ -52,6 +77,34 @@ namespace
         zed_net_address_t m_address;
         zed_net_udp_socket_t* m_handle;
     };
+
+    std::string GetBroadcastAddress()
+    {
+        std::string broadcast_string;
+
+        ifaddrs* devices = nullptr;
+        const int result = getifaddrs(&devices);
+        if(result != 0)
+            return broadcast_string;
+
+        for(ifaddrs* it = devices; it != nullptr; it = it->ifa_next)
+        {
+            if(!it->ifa_addr || it->ifa_addr->sa_family != AF_INET)
+                continue;
+
+            const bool loopback = (it->ifa_flags & IFF_LOOPBACK);
+            if(loopback)
+                continue;
+
+            const sockaddr_in* broadcast_addr = reinterpret_cast<const sockaddr_in*>(it->ifa_broadaddr);
+            broadcast_string = inet_ntoa(broadcast_addr->sin_addr);
+            break;
+        }
+
+        freeifaddrs(devices);
+
+        return broadcast_string;
+    }
 }
 
 
@@ -67,8 +120,37 @@ void Network::Exit()
     zed_net_shutdown();
 }
 
-std::shared_ptr<Network::ISocket> Network::OpenUDPSocket(const char* address, int port, bool blocking)
+std::string Network::GetLocalHostName()
 {
-    return std::make_shared<UDPSocket>(address, port, blocking);
+    constexpr int buffer_size = 128;
+    char buffer[buffer_size];
+    gethostname(buffer, buffer_size);
+
+    return buffer;
 }
 
+std::shared_ptr<Network::ISocket> Network::CreateUDPSocket(int port, bool blocking)
+{
+    return OpenUDPSocket(0, port, blocking);
+}
+
+std::shared_ptr<Network::ISocket> Network::OpenUDPSocket(const char* address, int port, bool blocking)
+{
+    try
+    {
+       return std::make_shared<UDPSocket>(address, port, blocking);
+    }
+    catch(const std::runtime_error& error)
+    {
+        std::printf("%s\n", error.what());
+        std::printf("zed_net_error: %s", zed_net_get_error());
+    }
+
+    return nullptr;
+}
+
+std::shared_ptr<Network::ISocket> Network::OpenBroadcastSocket(int port, bool blocking)
+{
+    const std::string& broadcast_string = GetBroadcastAddress();
+    return OpenUDPSocket(broadcast_string.c_str(), port, blocking);
+}
--- MonoEngine/System/SysNetwork.h
+++ MonoEngine/System/SysNetwork.h
@@ -11,6 +11,7 @@
 #include "SysTypes.h"
 #include <vector>
 #include <memory>
+#include <string>
 
 namespace Network
 {
@@ -19,6 +20,8 @@ namespace Network
 
     //! Exit the network module
     void Exit();
+
+    std::string GetLocalHostName();
 
 
     class ISocket
@@ -31,12 +34,16 @@ namespace Network
         //! Send data on the socket
         virtual int Send(const std::vector<byte>& data) = 0;
 
+        //! Send to a specific address and port
+        virtual int SendTo(const std::vector<byte>& data, const char* address, int port) = 0;
+
         //! Receives data from the socket into the buffer,
         //! make sure to reserve the amount of data you want to
         //! be able to receive.
         virtual void Receive(std::vector<byte>& buffer) = 0;
     };
 
-    //! Create a UDP socket...
+    std::shared_ptr<ISocket> CreateUDPSocket(int port, bool blocking);
     std::shared_ptr<ISocket> OpenUDPSocket(const char* address, int port, bool blocking);
+    std::shared_ptr<ISocket> OpenBroadcastSocket(int port, bool blocking);
 }
