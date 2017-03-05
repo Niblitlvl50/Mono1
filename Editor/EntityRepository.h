e40b7589931bffe2a08f3affad2e934b5ae79992
Niklas Damberg <niklas.damberg@gmail.com>
2017-03-05 23:41:49 +0100

    Load prefabs definition file

--- Editor/EntityRepository.h
+++ Editor/EntityRepository.h
@@ -15,15 +15,30 @@ namespace editor
         math::Vector scale;
     };
 
+    struct SnapPoint
+    {
+        math::Vector position;
+        float normal = 0;
+    };
+
+    struct PrefabDefinition
+    {
+        std::string name;
+        std::string sprite_file;
+        std::vector<math::Vector> collision_shape;
+        std::vector<SnapPoint> snap_points;
+    };
+
     class EntityRepository
     {
     public:
 
-        EntityRepository();
-
         bool LoadDefinitions();
+
         const EntityDefinition& GetDefinitionFromName(const std::string& name) const;
+        const PrefabDefinition& GetPrefabFromName(const std::string& name) const;
 
         std::vector<EntityDefinition> m_entities;
+        std::vector<PrefabDefinition> m_prefabs;
     };
 }
