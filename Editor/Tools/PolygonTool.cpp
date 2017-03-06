dfeb901728361cd7cde920032e11ace706a74a26
Niklas Damberg <niklas.damberg@gmail.com>
2017-03-06 09:21:32 +0100

    Fixed pop back in polygon tool and correct drawing

--- Editor/Tools/PolygonTool.cpp
+++ Editor/Tools/PolygonTool.cpp
@@ -32,8 +32,8 @@ public:
         DrawPolygon(renderer, m_texture, m_points, texture_points);
 
         constexpr mono::Color::RGBA color(1.0f, 0.0f, 0.0f, 0.2f);
-        const std::vector<math::Vector>& line = { m_points.front(), m_mousePosition };
-        renderer.DrawLines(line, color, 2.0f);
+        const std::vector<math::Vector>& line = { m_points.back(), m_mousePosition, m_points.front() };
+        renderer.DrawPolyline(line, color, 2.0f);
     }
 
     virtual math::Quad BoundingBox() const
@@ -70,10 +70,17 @@ bool PolygonTool::IsActive() const
 
 void PolygonTool::HandleContextMenu(int menu_index)
 {
+    if(m_points.empty())
+        return;
+
     if(menu_index == 0)
     {
         m_editor->AddPolygon(std::make_shared<editor::PolygonEntity>(m_points));
         m_points.clear();
+    }
+    else if(menu_index == 1)
+    {
+        m_points.pop_back();
     }
 }
 
