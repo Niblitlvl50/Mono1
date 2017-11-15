ff6e3c31892963ae63d078fe3373959863ab03fd
Niklas Damberg <niklas.damberg@gmail.com>
2017-11-15 12:03:45 +0100

    Fixed the string split function and tests.

--- MonoEngine/StringFunctions.cpp
+++ MonoEngine/StringFunctions.cpp
@@ -5,15 +5,19 @@ std::vector<std::string> mono::SplitString(const std::string& string, char delim
 {
     std::vector<std::string> tokens;
 
-    size_t search_position = 0;
+    size_t start_position = 0;
+    size_t find_position = std::string::npos;
 
     do {
-        const size_t pos = string.find_first_of(delimiter, search_position);
-        if(pos != std::string::npos)
-            tokens.push_back(string.substr(search_position, pos));
-        
-        search_position = pos;
-    } while(search_position != std::string::npos);
+        find_position = string.find_first_of(delimiter, start_position);
+        if(find_position != std::string::npos)
+        {
+            tokens.push_back(string.substr(start_position, find_position - start_position));
+            start_position = find_position +1;
+        }
+    } while(find_position != std::string::npos);
+
+    tokens.push_back(string.substr(start_position));
 
     return tokens;
 }
--- UnitTests/StringFunctionsTest.cpp
+++ UnitTests/StringFunctionsTest.cpp
@@ -2,14 +2,22 @@
 #include "StringFunctions.h"
 #include <gtest/gtest.h>
 
-TEST(StringFunctionsTest, Split)
+TEST(StringFunctionsTest, SplitMultipleTokens)
 {
-    constexpr const char* string_1 = "hello banan boat!";
-
+    constexpr const char* string_1 = "hello banana boat!";
     const std::vector<std::string>& tokens = mono::SplitString(string_1, ' ');
 
-    EXPECT_EQ(size_t(3), tokens.size());
+    EXPECT_EQ(3u, tokens.size());
     EXPECT_STREQ("hello", tokens[0].c_str());
     EXPECT_STREQ("banana", tokens[1].c_str());
     EXPECT_STREQ("boat!", tokens[2].c_str());
+}
+
+TEST(StringFunctionsTest, SplitSingleToken)
+{
+    constexpr const char* string_1 = "whatever";
+    const std::vector<std::string>& tokens = mono::SplitString(string_1, ' ');
+
+    EXPECT_EQ(1u, tokens.size());
+    EXPECT_STREQ(string_1, tokens[0].c_str());
 }
