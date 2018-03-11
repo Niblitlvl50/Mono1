9e21503a151a149dd53c0343e8aaccc5ee95b623
Niklas Damberg <niklas.damberg@gmail.com>
2018-03-11 15:22:11 +0100

    Made the particle emitter more reliable and exact. Also added burst mode.

--- MonoEngine/Particle/ParticleEmitter.cpp
+++ MonoEngine/Particle/ParticleEmitter.cpp
@@ -7,9 +7,12 @@
 using namespace mono;
 
 ParticleEmitter::ParticleEmitter(const Configuration& config, ParticlePool& pool)
-    : m_config(config),
-      m_position(config.position),
-      m_pool(pool)
+    : m_config(config)
+    , m_burst_emitted(false)
+    , m_duration(0.0f)
+    , m_carry_over(0.0f)
+    , m_position(config.position)
+    , m_pool(pool)
 { }
 
 void ParticleEmitter::SetPosition(const math::Vector& position)
@@ -17,9 +20,37 @@ void ParticleEmitter::SetPosition(const math::Vector& position)
     m_position = position;
 }
 
+void ParticleEmitter::Stop()
+{
+    m_duration = m_config.duration;
+}
+
 void ParticleEmitter::doUpdate(unsigned int delta)
 {
-    const size_t new_particles = std::max(static_cast<size_t>(float(delta) * m_config.emit_rate), size_t(1));
+    if(m_config.burst && m_burst_emitted)
+        return;
+
+    if(m_config.duration > 0.0f && m_duration > m_config.duration)
+        return;
+
+    m_duration += float(delta) / 1000.0f;
+
+    size_t new_particles = 0;
+    
+    if(m_config.burst)
+    {
+        new_particles = m_config.emit_rate * m_config.duration;
+    }
+    else
+    {
+        const float delta_second = float(delta) / 1000.0f;
+        new_particles = static_cast<size_t>(delta_second * (m_config.emit_rate + m_carry_over));
+        if(new_particles == 0)
+            m_carry_over += m_config.emit_rate;
+        else
+            m_carry_over = 0.0f;
+    }
+
     const size_t start_index = m_pool.m_count_alive;
     const size_t end_index = std::min(start_index + new_particles, m_pool.m_pool_size -1);
 
@@ -28,4 +59,6 @@ void ParticleEmitter::doUpdate(unsigned int delta)
 
     for(size_t index = start_index; index < end_index; ++index)
         m_pool.Wake(index);
+
+    m_burst_emitted = true;
 }
--- MonoEngine/Particle/ParticleEmitter.h
+++ MonoEngine/Particle/ParticleEmitter.h
@@ -15,19 +15,26 @@ namespace mono
         struct Configuration
         {
             math::Vector position;
-            float emit_rate = 0.2f;
+            float duration = 5.0f;      // seconds, negative value means infinite
+            float emit_rate = 10.0f;    // particles per second
+            bool burst = false;         // if true, one burst only and will emit duration * emit_rate particles
             ParticleGenerator generator = nullptr;
             const void* generator_context = nullptr;
         };
 
         ParticleEmitter(const Configuration& config, ParticlePool& pool);
         void SetPosition(const math::Vector& position);
+        void Stop();
 
         void doUpdate(unsigned int delta) override;
 
     private:
     
         const Configuration m_config;
+        bool m_burst_emitted;
+        float m_duration;
+        float m_carry_over;
+
         math::Vector m_position;
         ParticlePool& m_pool;
     };
