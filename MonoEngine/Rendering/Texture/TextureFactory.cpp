0ed5f3f2713c718fdfefc03572f62599a8b0fdb3
Niklas Damberg <niklas.damberg@gmail.com>
2018-09-12 21:38:32 +0200

    Updated the internals of texture factory, you can now cache created textures from data.

--- MonoEngine/Rendering/Texture/TextureFactory.cpp
+++ MonoEngine/Rendering/Texture/TextureFactory.cpp
@@ -11,46 +11,66 @@
 namespace
 {
     // This is where all the weak pointers goes, that points to the allocated textures!
-    std::unordered_map<std::string, std::weak_ptr<mono::ITexture>> textureStorage;
+    std::unordered_map<std::string, std::weak_ptr<mono::ITexture>> g_texture_storage;
+
+    mono::ITexturePtr GetTextureFromCache(const char* texture_name)
+    {
+        auto it = g_texture_storage.find(texture_name);
+        if(it != g_texture_storage.end())
+        {
+            mono::ITexturePtr texture = it->second.lock();
+            if(texture)
+                return texture;
+        }
+
+        std::printf("TextureFactory - Unable to find '%s' in cache.\n", texture_name);
+        return nullptr;
+    }
+
+    mono::ITexturePtr CreateAndCacheTexture(const System::IImagePtr& image, const char* texture_name)
+    {
+        const auto deleter = [texture_name](mono::ITexture* ptr) {
+            g_texture_storage.erase(texture_name);
+            delete ptr;
+        };
+
+        mono::ITexturePtr texture(new mono::Texture(image), deleter);
+        g_texture_storage[texture_name] = texture;
+
+        return texture;
+    }
 }
 
 mono::ITexturePtr mono::CreateTexture(const char* source)
 {
-    auto it = textureStorage.find(source);
-    if(it != textureStorage.end())
+    mono::ITexturePtr texture = GetTextureFromCache(source);
+    if(texture)
+        return texture;
+    
+    const System::IImagePtr image = System::LoadImage(source);
+    return CreateAndCacheTexture(image, source);
+}
+
+mono::ITexturePtr mono::CreateTextureFromData(const unsigned char* data, int data_length, const char* cache_name)
+{
+    if(cache_name)
     {
-        mono::ITexturePtr texture = it->second.lock();
+        mono::ITexturePtr texture = GetTextureFromCache(cache_name);
         if(texture)
             return texture;
 
-        std::printf("TextureFactory - Recreating '%s'\n", source);
+        const System::IImagePtr image = System::LoadImageFromData(data, data_length);
+        return CreateAndCacheTexture(image, cache_name);
     }
-    
-    const System::IImagePtr image = System::LoadImage(source);
-
-    // Custom deleter that erases the texture from the map
-    // when all the references are gone.
-    const auto deleter = [source](mono::ITexture* ptr) {
-        textureStorage.erase(source);
-        delete ptr;
-    };
-
-    mono::ITexturePtr texture(new Texture(image), deleter);
-    textureStorage[source] = texture;
-    
-    return texture;
+    else
+    {
+        const System::IImagePtr image = System::LoadImageFromData(data, data_length);
+        return std::make_shared<Texture>(image);    
+    }
 }
 
-mono::ITexturePtr mono::CreateTextureFromData(const unsigned char* data, int data_length)
+mono::ITexturePtr mono::CreateTexture(const unsigned char* data, int width, int height, int color_components)
 {
-    const System::IImagePtr image = System::LoadImageFromData(data, data_length);
-    return std::make_shared<Texture>(image);    
-}
-
-mono::ITexturePtr mono::CreateTexture(const unsigned char* data, int width, int height, int colorComponents)
-{
-    const System::IImagePtr image = System::CreateImage(data, width, height, colorComponents);
+    const System::IImagePtr image = System::CreateImage(data, width, height, color_components);
     return std::make_shared<Texture>(image);
 }
-
-
--- MonoEngine/Rendering/Texture/TextureFactory.h
+++ MonoEngine/Rendering/Texture/TextureFactory.h
@@ -5,12 +5,13 @@
 
 namespace mono
 {
-    class ITexture;
-    using ITexturePtr = std::shared_ptr<ITexture>;
+    using ITexturePtr = std::shared_ptr<class ITexture>;
 
     //! Create a texture from disk, only PNG is supported at the moment
     ITexturePtr CreateTexture(const char* source);
-    ITexturePtr CreateTextureFromData(const unsigned char* data, int data_length);
+
+    // 'cache_name' can be nullptr, then it will not be stored in the cache.
+    ITexturePtr CreateTextureFromData(const unsigned char* data, int data_length, const char* cache_name);
 
     //! Create a texture from memory
     ITexturePtr CreateTexture(const unsigned char* data, int width, int height, int colorComponents);
