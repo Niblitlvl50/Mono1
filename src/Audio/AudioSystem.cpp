6fc4ee770c62cf57e845b38d843a0b268b665945
Niklas Damberg <niklas.damberg@gmail.com>
2020-01-18 21:17:08 +0100

    Moved functions.

--- src/Audio/AudioSystem.cpp
+++ src/Audio/AudioSystem.cpp
@@ -43,3 +43,13 @@ void mono::ShutdownAudio()
     alcDestroyContext(context);
     alcCloseDevice(device);
 }
+
+void mono::ListenerPosition(float x, float y)
+{
+    alListener3f(AL_POSITION, x, y, 0.0f);
+}
+
+void mono::ListenerGain(float gain)
+{
+    alListenerf(AL_GAIN, gain);
+}
--- src/Audio/AudioSystem.h
+++ src/Audio/AudioSystem.h
@@ -5,4 +5,10 @@ namespace mono
 {
     void InitializeAudio();
     void ShutdownAudio();
+
+    // Set the listener position
+    void ListenerPosition(float x, float y);
+
+    // Set the listener volume (gain)
+    void ListenerGain(float gain);
 }
