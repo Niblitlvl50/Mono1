9ea530cac7a60b0afa5d28983560255101ac26a6
Niklas Damberg <niklas.damberg@gmail.com>
2020-04-10 09:46:35 +0200

    ZonePtr is unique.

--- src/MonoPtrFwd.h
+++ src/MonoPtrFwd.h
@@ -9,7 +9,7 @@ namespace mono
     using ICameraPtr = std::shared_ptr<ICamera>;
     using IEntityPtr = std::shared_ptr<IEntity>;
     using IUpdatablePtr = std::shared_ptr<IUpdatable>;
-    using IZonePtr = std::shared_ptr<IZone>;
+    using IZonePtr = std::unique_ptr<IZone>;
     using ISoundPtr = std::unique_ptr<ISound>;
     using IPathPtr = std::unique_ptr<IPath>;
 }
