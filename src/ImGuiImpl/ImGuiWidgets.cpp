150af4bec5e83f335aca22189819352580ae155b
Niklas Damberg <niklas.damberg@gmail.com>
2020-10-16 13:13:38 +0200

    Tiny fix for imgui widget.

--- src/ImGuiImpl/ImGuiWidgets.cpp
+++ src/ImGuiImpl/ImGuiWidgets.cpp
@@ -3,18 +3,18 @@
 #include "ImGuiImpl.h"
 #include <string>
 
-void mono::DrawBitFieldType(
-    uint32_t& properties, const uint32_t* possible_properties, uint32_t n_properties, mono::BitFieldTextCallback text_callback)
+bool mono::DrawBitFieldType(
+    uint32_t& value, const uint32_t* possible_properties, uint32_t n_properties, mono::BitFieldTextCallback text_callback)
 {
     std::string button_text = std::string("[") + text_callback(0) + "]";
 
-    if(properties != 0)
+    if(value != 0)
     {
         button_text.clear();
         for(uint32_t prop_index = 0; prop_index < n_properties; ++prop_index)
         {
             const uint32_t prop = possible_properties[prop_index];
-            if(properties & prop)
+            if(value & prop)
             {
                 button_text += text_callback(prop);
                 button_text += "|";
@@ -28,15 +28,19 @@ void mono::DrawBitFieldType(
     if(pushed)
         ImGui::OpenPopup("entity_properties_select");
 
+    const uint32_t initial_value = value;
+
     if(ImGui::BeginPopup("entity_properties_select"))
     {
         for(uint32_t prop_index = 0; prop_index < n_properties; ++prop_index)
         {
             const uint32_t prop = possible_properties[prop_index];
-            if(ImGui::Selectable(text_callback(prop), properties & prop, ImGuiSelectableFlags_DontClosePopups))
-                properties ^= prop;
+            if(ImGui::Selectable(text_callback(prop), value & prop, ImGuiSelectableFlags_DontClosePopups))
+                value ^= prop;
         }
 
         ImGui::EndPopup();
     }
+
+    return initial_value != value;
 }
--- src/ImGuiImpl/ImGuiWidgets.h
+++ src/ImGuiImpl/ImGuiWidgets.h
@@ -6,6 +6,6 @@
 namespace mono
 {
     using BitFieldTextCallback = const char*(*)(uint32_t);
-    void DrawBitFieldType(
-        uint32_t& properties, const uint32_t* possible_properties, uint32_t n_properties, BitFieldTextCallback text_callback);
+    bool DrawBitFieldType(
+        uint32_t& value, const uint32_t* possible_properties, uint32_t n_properties, BitFieldTextCallback text_callback);
 }
