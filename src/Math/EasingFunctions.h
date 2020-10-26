1f81ed435dbdfaf3efa034a0de138238b7dc3d2b
Niklas Damberg <niklas.damberg@gmail.com>
2020-10-26 12:57:42 +0100

    Added easing functions.

--- src/Math/EasingFunctions.h
+++ src/Math/EasingFunctions.h
@@ -39,6 +39,32 @@ namespace math
         return delta_value / 2.0f * (time * time * time + 2.0f) + start_value;
     };
 
+    inline float EaseBackIn(float time, float duration, float start_value, float delta_value)
+    {
+        time /= duration;
+        constexpr float s = 1.70158f;
+        return delta_value * time * time * ((s + 1.0f) * time - s) + start_value;
+    }
+
+    inline float EaseBackOut(float time, float duration, float start_value, float delta_value)
+    {
+        constexpr float s = 1.70158f;
+        const float t0 = time / duration - 1.0f;
+        return delta_value * (t0 * t0 * ((s + 1.0f) * t0 + s) + 1.0f) + start_value;
+    }
+
+    inline float EaseBackInOut(float time, float duration, float start_value, float delta_value)
+    {
+        constexpr float s = 1.70158f;
+        const float s0 = s * 1.525f;
+        float t0 = time / (duration / 2.0f);
+        if (t0 < 1.0f)
+            return delta_value / 2.0f * (t0 * t0 * ((s0 + 1.0f) * t0 - s0)) + start_value;
+        
+        t0 -= 2.0f;
+        return delta_value / 2.0f * (t0 * t0 * ((s0 + 1.0f) * t0 + s0) + 2.0f) + start_value;
+    }
+
     using EaseFunction = float (*)(float time, float duration, float start_value, float delta_value);
 
     constexpr EaseFunction ease_functions[] = {
@@ -46,6 +72,9 @@ namespace math
         EaseInCubic,
         EaseOutCubic,
         EaseInOutCubic,
+        EaseBackIn,
+        EaseBackOut,
+        EaseBackInOut,
     };
 
     enum class EasingFuncType
@@ -54,6 +83,9 @@ namespace math
         EASE_IN_CUBIC,
         EASE_OUT_CUBIC,
         EASE_IN_OUT_CUBIC,
+        EASE_BACK_IN,
+        EASE_BACK_OUT,
+        EASE_BACK_IN_OUT,
     };
 
     constexpr const char* easing_function_strings[] = {
@@ -61,6 +93,9 @@ namespace math
         "Ease In Cubic",
         "Ease Out Cubic",
         "Ease In/Out Cubic",
+        "Ease Back In",
+        "Ease Back Out",
+        "Ease Back In Out",
     };
 
     inline const char* EasingFuncTypeToString(EasingFuncType logic_type)
