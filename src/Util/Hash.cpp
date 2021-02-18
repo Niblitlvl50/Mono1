24db570bab6ce6eaeb3bfc21d5549055a7cc05a4
Niblitlvl50 <niklas.damberg@gmail.com>
2021-02-18 21:03:26 +0100

    Added hash lookup and register.

---
+++ src/Util/Hash.cpp
@@ -0,0 +1,23 @@
+
+#include "Hash.h"
+#include <unordered_map>
+#include <string>
+
+namespace
+{
+    std::unordered_map<uint32_t, std::string> g_hash_register;
+}
+
+void mono::HashRegister(uint32_t hash_value, const char* string)
+{
+    g_hash_register[hash_value] = string;
+}
+
+const char* mono::HashLookup(uint32_t hash_value)
+{
+    const auto it = g_hash_register.find(hash_value);
+    if(it != g_hash_register.end())
+        return it->second.c_str();
+
+    return "unknown";
+}
--- src/Util/Hash.h
+++ src/Util/Hash.h
@@ -9,12 +9,11 @@ namespace mono
     //
     // This is the "FNV-1a alternate algorithm", taken from here: http://isthe.com/chongo/tech/comp/fnv/
     //
-    inline uint32_t Hash(const char* text)
+    inline uint32_t Hash(const char* text, uint32_t length)
     {
         constexpr uint32_t offset_bias = 2166136261u;
         constexpr uint32_t FNV_prime = 16777619u;
 
-        const std::size_t length = std::strlen(text);
         uint32_t hash = offset_bias;
 
         for(uint32_t index = 0; index < length; ++index)
@@ -22,4 +21,13 @@ namespace mono
 
         return hash;
     }
+
+    inline uint32_t Hash(const char* text)
+    {
+        const std::size_t length = std::strlen(text);
+        return Hash(text, length);
+    }
+
+    void HashRegister(uint32_t hash_value, const char* string);
+    const char* HashLookup(uint32_t hash_value);
 }
