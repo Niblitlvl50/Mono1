9a9d2331eeba85cb4f53219aea099b8d27afb11f
Niblitlvl50 <niklas.damberg@gmail.com>
2021-05-25 14:25:06 +0200

    Fix bounding box for text.

--- src/Rendering/Text/TextSystem.cpp
+++ src/Rendering/Text/TextSystem.cpp
@@ -52,10 +52,17 @@ void TextSystem::Update(const mono::UpdateContext& update_context)
         if(!m_text_dirty[index])
             return;
 
-        const math::Vector size = mono::MeasureString(text.font_id, text.text.c_str());
-        
+        math::Vector text_offset;
+        const math::Vector text_size = mono::MeasureString(text.font_id, text.text.c_str());
+        const math::Vector half_size = text_size / 2.0f;
+
+        if(text.center_flags & FontCentering::HORIZONTAL)
+            text_offset.x = half_size.x;
+        if(text.center_flags & FontCentering::VERTICAL)
+            text_offset.y = half_size.y;
+
         math::Quad& bounding_box = m_transform_system->GetBoundingBox(index);
-        bounding_box = math::Quad(math::ZeroVec, size);
+        bounding_box = math::Quad(-text_offset, text_size - text_offset);
 
         m_text_dirty[index] = false;
     };
