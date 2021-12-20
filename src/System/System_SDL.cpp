55f2327228f442f16b07073479fa930da1ea546d
Niblitlvl50 <niklas.damberg@gmail.com>
2021-12-20 14:30:18 +0100

    Try to set adaptive vsync, otherwise set vsync.

--- src/System/System_SDL.cpp
+++ src/System/System_SDL.cpp
@@ -74,8 +74,13 @@ namespace
                 throw std::runtime_error("System|Unable to create OpenGL context. (" + sdl_error + ")");
             }
 
-            const int vsync_value = (options & System::WindowOptions::DISABLE_VSYNC) ? 0 : 1;
-            SDL_GL_SetSwapInterval(vsync_value);
+            const int swap_result = SDL_GL_SetSwapInterval(-1);
+            if(swap_result != 0)
+            {
+                System::Log("System|Adaptive vsync not supported.");
+                const int vsync_value = (options & System::WindowOptions::DISABLE_VSYNC) ? 0 : 1;
+                SDL_GL_SetSwapInterval(vsync_value);
+            }
 
             MakeCurrent();
         }
