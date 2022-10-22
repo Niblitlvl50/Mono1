d410fa8e12f2c75aa2abab4ccfcb848e318b74a3
Niblitlvl50 <niklas.damberg@gmail.com>
2022-10-22 21:01:35 +0200

    Random stuff.

--- src/Util/Random.cpp
+++ src/Util/Random.cpp
@@ -2,20 +2,36 @@
 #include "Random.h"
 #include <random>
 
+using namespace mono;
+
+RandomGenerator::RandomGenerator(int seed)
+    : m_random_engine(seed)
+{ }
+
+float RandomGenerator::GetFloat(float min, float max)
+{
+    std::uniform_real_distribution<float> distribution(min, max);
+    return distribution(m_random_engine);
+}
+
+int RandomGenerator::GetInteger(int min, int max)
+{
+    std::uniform_int_distribution<int> distribution(min, max);
+    return distribution(m_random_engine);
+}
+
 namespace
 {
     constexpr int seed = 666;
-    static std::default_random_engine engine(seed);
+    static RandomGenerator g_random_generator(seed);
 }
 
 float mono::Random(float min, float max)
 {
-    std::uniform_real_distribution<float> distribution(min, max);
-    return distribution(engine);
+    return g_random_generator.GetFloat(min, max);
 }
 
 int mono::RandomInt(int min, int max)
 {
-    std::uniform_int_distribution<int> distribution(min, max);
-    return distribution(engine);
+    return g_random_generator.GetInteger(min, max);
 }
--- src/Util/Random.h
+++ src/Util/Random.h
@@ -1,13 +1,23 @@
 
 #pragma once
 
+#include <random>
 #include <cstdint>
 
 namespace mono
 {
-    //! Generates a random float between min and max
-    float Random(float min = 0.0f, float max = 1.0f);
+    class RandomGenerator
+    {
+    public:
+        RandomGenerator(int seed);
+        float GetFloat(float min = 0.0f, float max = 1.0f);
+        int GetInteger(int min = 0, int max = 100);
 
+    private:
+        std::default_random_engine m_random_engine;
+    };
+
+    float Random(float min = 0.0f, float max = 1.0f);
     int RandomInt(int min = 0, int max = 100);
 
     // Specify the percentage for the outcome to be true, 100 means always true, 20 means 20% of the time its true.
@@ -22,6 +32,10 @@ namespace mono
 
         using result_type = uint32_t;
 
+        UniformRandomBitGenerator(int seed)
+            : m_random_generator(seed)
+        { }
+
         static constexpr result_type min()
         {
             return 0;
@@ -34,7 +48,9 @@ namespace mono
 
         result_type operator()()
         {
-            return RandomInt(min(), max());
+            return m_random_generator.GetInteger(min(), max());
         }
+
+        RandomGenerator m_random_generator;
     };
 }
