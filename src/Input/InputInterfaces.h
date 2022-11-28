0a3662ddbac2d06c50a4af7267742b7b207fe8c5
Niblitlvl50 <niklas.damberg@gmail.com>
2022-11-28 20:53:27 +0100

    Moved out interfaces

---
+++ src/Input/InputInterfaces.h
@@ -0,0 +1,45 @@
+
+#pragma once
+
+#include "EventHandler/EventToken.h"
+#include "Events/MouseEvent.h"
+#include "Events/ControllerEvent.h"
+#include "Events/KeyEvent.h"
+
+namespace mono
+{
+    enum class InputResult
+    {
+        Handled,
+        Pass
+    };
+
+    class IMouseInput
+    {
+    public:
+
+        virtual ~IMouseInput() = default;
+        virtual mono::InputResult Move(const event::MouseMotionEvent& event) { return mono::InputResult::Pass; };
+        virtual mono::InputResult ButtonDown(const event::MouseDownEvent& event) { return mono::InputResult::Pass; };
+        virtual mono::InputResult ButtonUp(const event::MouseUpEvent& event) { return mono::InputResult::Pass; };
+        virtual mono::InputResult Wheel(const event::MouseWheelEvent& event) { return mono::InputResult::Pass; };
+    };
+
+    class IControllerInput
+    {
+    public:
+
+        virtual ~IControllerInput() = default;
+        virtual mono::InputResult ButtonDown(const event::ControllerButtonDownEvent& event) { return mono::InputResult::Pass; };
+        virtual mono::InputResult Axis(const event::ControllerAxisEvent& event) { return mono::InputResult::Pass; };
+    };
+
+    class IKeyboardInput
+    {
+    public:
+
+        virtual ~IKeyboardInput() = default;
+        virtual mono::InputResult KeyDown(const event::KeyDownEvent& event) { return mono::InputResult::Pass; };
+        virtual mono::InputResult KeyUp(const event::KeyUpEvent& event) { return mono::InputResult::Pass; };
+    };
+}
--- src/Input/InputSystem.h
+++ src/Input/InputSystem.h
@@ -3,52 +3,12 @@
 
 #include "MonoFwd.h"
 #include "IGameSystem.h"
-
-#include "EventHandler/EventToken.h"
-#include "Events/MouseEvent.h"
-#include "Events/ControllerEvent.h"
-#include "Events/KeyEvent.h"
+#include "InputInterfaces.h"
 
 #include <vector>
 
 namespace mono
 {
-    enum class InputResult
-    {
-        Handled,
-        Pass
-    };
-
-    class IMouseInput
-    {
-    public:
-
-        virtual ~IMouseInput() = default;
-        virtual mono::InputResult Move(const event::MouseMotionEvent& event) { return mono::InputResult::Pass; };
-        virtual mono::InputResult ButtonDown(const event::MouseDownEvent& event) { return mono::InputResult::Pass; };
-        virtual mono::InputResult ButtonUp(const event::MouseUpEvent& event) { return mono::InputResult::Pass; };
-        virtual mono::InputResult Wheel(const event::MouseWheelEvent& event) { return mono::InputResult::Pass; };
-    };
-
-    class IControllerInput
-    {
-    public:
-
-        virtual ~IControllerInput() = default;
-        virtual mono::InputResult ButtonDown(const event::ControllerButtonDownEvent& event) { return mono::InputResult::Pass; };
-        virtual mono::InputResult Axis(const event::ControllerAxisEvent& event) { return mono::InputResult::Pass; };
-    };
-
-    class IKeyboardInput
-    {
-    public:
-
-        virtual ~IKeyboardInput() = default;
-        virtual mono::InputResult KeyDown(const event::KeyDownEvent& event) { return mono::InputResult::Pass; };
-        virtual mono::InputResult KeyUp(const event::KeyUpEvent& event) { return mono::InputResult::Pass; };
-    };
-
-
     enum class InputContextBehaviour
     {
         ConsumeIfHandled,
