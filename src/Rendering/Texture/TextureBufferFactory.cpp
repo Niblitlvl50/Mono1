5175a59ef8ad602bc9319b6161427457ecd72fa3
niblitlvl50 <niklas.damberg@gmail.com>
2024-07-09 11:08:16 +0200

    Some anchoring for textures.

--- src/Rendering/Texture/TextureBufferFactory.cpp
+++ src/Rendering/Texture/TextureBufferFactory.cpp
@@ -5,16 +5,18 @@
 #include "Rendering/Texture/ITexture.h"
 #include "Math/Vector.h"
 
-mono::TextureDrawBuffers mono::BuildTextureDrawBuffers(const mono::ITexture* texture)
+mono::TextureDrawBuffers mono::BuildTextureDrawBuffers(const mono::ITexture* texture, const mono::TextureBufferOptions& options)
 {
-    const float half_width = texture->Width() / 2.0f;
-    const float half_height = texture->Height() / 2.0f;
+    const float half_width = (texture->Width() / 2.0f) / options.pixels_per_meter;
+    const float half_height = (texture->Height() / 2.0f) / options.pixels_per_meter;
+    
+    const math::Vector shift_vector = mono::CalculateOffsetFromCenter(options.anchor, math::Vector(texture->Width(), texture->Height()), options.pixels_per_meter);
 
     const math::Vector vertex_data[] = {
-        { -half_width, -half_height },
-        { -half_width, +half_height },
-        { +half_width, +half_height },
-        { +half_width, -half_height }
+        math::Vector(-half_width, -half_height) + shift_vector,
+        math::Vector(-half_width, +half_height) + shift_vector,
+        math::Vector(+half_width, +half_height) + shift_vector,
+        math::Vector(+half_width, -half_height) + shift_vector,
     };
 
     const math::Vector uv_data[] = {
@@ -35,4 +37,32 @@ mono::TextureDrawBuffers mono::BuildTextureDrawBuffers(const mono::ITexture* tex
     draw_buffers.indices = mono::CreateElementBuffer(mono::BufferType::STATIC, 6, index_data);
 
     return draw_buffers;
+}
+
+math::Vector mono::CalculateOffsetFromCenter(mono::AnchorPoint anchor_point, const math::Vector& texture_size, float pixels_per_meter)
+{
+    const float half_width = (texture_size.x / 2.0f) / pixels_per_meter;
+    const float half_height = (texture_size.y / 2.0f) / pixels_per_meter;
+
+    math::Vector shift_vector = math::ZeroVec;
+
+    switch(anchor_point)
+    {
+    case mono::AnchorPoint::CENTER:
+        break;
+    case mono::AnchorPoint::TOP_LEFT:
+        shift_vector = math::Vector(half_width, -half_height);
+        break;
+    case mono::AnchorPoint::BOTTOM_LEFT:
+        shift_vector = math::Vector(half_width, half_height);
+        break;
+    case mono::AnchorPoint::TOP_RIGHT:
+        shift_vector = math::Vector(-half_width, -half_height);
+        break;
+    case mono::AnchorPoint::BOTTOM_RIGHT:
+        shift_vector = math::Vector(-half_width, half_height);
+        break;
+    }
+
+    return shift_vector;
 }
--- src/Rendering/Texture/TextureBufferFactory.h
+++ src/Rendering/Texture/TextureBufferFactory.h
@@ -1,7 +1,9 @@
 
 #pragma once
 
+#include "Math/MathFwd.h"
 #include "Rendering/RenderFwd.h"
+
 #include <memory>
 
 namespace mono
@@ -13,5 +15,21 @@ namespace mono
         std::unique_ptr<IElementBuffer> indices;
     };
 
-    TextureDrawBuffers BuildTextureDrawBuffers(const mono::ITexture* texture);
+    enum class AnchorPoint
+    {
+        CENTER,
+        TOP_LEFT,
+        BOTTOM_LEFT,
+        TOP_RIGHT,
+        BOTTOM_RIGHT
+    };
+
+    struct TextureBufferOptions
+    {
+        AnchorPoint anchor = AnchorPoint::CENTER;
+        float pixels_per_meter = 1.0f;
+    };
+
+    TextureDrawBuffers BuildTextureDrawBuffers(const mono::ITexture* texture, const TextureBufferOptions& options);
+    math::Vector CalculateOffsetFromCenter(mono::AnchorPoint anchor_point, const math::Vector& texture_size, float pixels_per_meter);
 }
